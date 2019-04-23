84b1e786c86b3aac9fd7bc7105b32808bc5a2147
Mohammad Ali Riazati <47595182+riazati75@users.noreply.github.com>
2019-04-23 12:08:13 +0430

    Initial commit
    Initial commit

--- app/src/main/java/com/ticka/application/fragments/CapacityFragment.java
+++ app/src/main/java/com/ticka/application/fragments/CapacityFragment.java
@@ -1,23 +1,55 @@
 package com.ticka.application.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.ticka.application.R;
+import com.ticka.application.widgets.ValueChanger;
 
 public class CapacityFragment extends Fragment {
+
+    private Context context;
+    private TextView ca1 , ca2 , ca3 , ca4;
+    private ValueChanger c1 , c2 , c3 , c4;
 
     public CapacityFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_capacity, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_capacity, container, false);
+        context = container.getContext();
+
+        initViews(view);
+        init();
+
+        return view;
+    }
+
+    private void initViews(View view){
+
+        ca1 = view.findViewById(R.id.ca1);
+        ca2 = view.findViewById(R.id.ca2);
+        ca3 = view.findViewById(R.id.ca3);
+        ca4 = view.findViewById(R.id.ca4);
+
+        ca1.setText(R.string.capacity_1);
+        ca2.setText(R.string.capacity_2);
+        ca3.setText(R.string.capacity_3);
+        ca4.setText(R.string.capacity_4);
+
+    }
+
+    private void init(){
+
     }
 
 }
