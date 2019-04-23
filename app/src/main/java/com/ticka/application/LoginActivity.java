ec9495417ca79f07c884fb238afb5c2c8117b36f
Mohammad Ali Riazati <47595182+riazati75@users.noreply.github.com>
2019-04-23 13:12:03 +0430

    Initial commit
    Initial commit

---
+++ app/src/main/java/com/ticka/application/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.ticka.application;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import ir.aid.library.Frameworks.setup.SetupActivity;
+
+public class LoginActivity extends SetupActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+    
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(newBase);
+    }
+}
